47d4c168273cfa158cc67f43526a8fe9ec7c7bd2
Charlie <sgvvannabe@gmail.com>
2016-02-23 23:55:39 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Quizone::Application.config.secret_key_base = '94652fb494554cdb15d5b80eea72951d559518ca93b9291c9ab6f725df974106d271411c9917b04317e4b1afae863b64416f1a8d877099d872f8b891f5ee1718'
